d8ace96944ef0bbd846060ccbc65ad0c802d4f1f
Frappe User <frappe@DESKTOP-KO8SP4E.localdomain>
2025-10-19 12:06:33 -0600

    fix: resolve syntax errors - remove invalid shell commands from setup.py and fix tasks.py indentation

--- setup.py
+++ setup.py
@@ -13,12 +13,3 @@ setup(
         "frappe",
     ],
 )
-# Add setup.py to each repository's root
-echo "from setuptools import setup, find_packages
-setup(
-    name='repo_name',
-    version='0.1.0',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=['frappe']
-)" > setup.py
