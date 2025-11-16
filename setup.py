356c085e50163b5fcf87c16a49e644e327b2b48b
Rogelio Pedroza Duarte <rogerboy38@hotmail.com>
2025-11-16 07:59:29 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="rnd_nutrition",
     version="0.0.1",
     description="RND Nutrition App for ERPNext",
-    author="Your Name",
-    author_email="your@email.com",
+    author="Rogerboy38",
+    author_email="rogerboy38@hotmail.com",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
@@ -13,12 +13,4 @@ setup(
         "frappe",
     ],
 )
-# Add setup.py to each repository's root
-echo "from setuptools import setup, find_packages
-setup(
-    name='repo_name',
-    version='0.1.0',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=['frappe']
-)" > setup.py
+
